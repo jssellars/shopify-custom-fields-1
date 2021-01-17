e30d3c51793ba0c92787ed32553905e7ebbd669f
markmead <itsmarkmead@gmail.com>
2021-01-17 22:23:00 +0000

    replace function with npm package

--- src/app/components/button-copy-to-clipboard/button-copy-to-clipboard.component.ts
+++ src/app/components/button-copy-to-clipboard/button-copy-to-clipboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import PrettyHTML from 'format-string-to-html';
 
 @Component({
   selector: 'app-button-copy-to-clipboard',
@@ -11,37 +12,10 @@ export class ButtonCopyToClipboardComponent implements OnInit {
 
   ngOnInit(): void { }
 
-  private prettyHTML(htmlString: string): string {
-    const commentRegex = /(<!--.*?-->)|(<!--[\S\s]+?-->)|(<!--[\S\s]*?$)/g;
-    const htmlTab = '\t';
-    let htmlIndent = '';
-    let htmlResult = '';
-
-    if (htmlString) {
-      htmlString = htmlString.replace(commentRegex, '');
-
-      const htmlArray = htmlString.split(/>\s*</);
-
-      for (const htmlElement of htmlArray) {
-        if (htmlElement.match(/^\/\w/)) {
-          htmlIndent = htmlIndent.substring(htmlTab.length);
-        }
-
-        htmlResult += `${htmlIndent}<${htmlElement}>\r\n`;
-
-        if (htmlElement.match(/^<?\w[^>]*[^\/]$/) && !htmlElement.startsWith('input')) {
-          htmlIndent += htmlTab;
-        }
-      }
-    }
-
-    return htmlResult.substring(1, htmlResult.length - 3);
-  }
-
   public copyHTMLToClipboard(): void {
     const fakeInput = document.createElement('textarea');
     const htmlString = this.inputHTML.toString();
-    const formattedHTML = this.prettyHTML(htmlString);
+    const formattedHTML = new PrettyHTML(htmlString).format();
 
     if (formattedHTML) {
       fakeInput.innerHTML = formattedHTML;
